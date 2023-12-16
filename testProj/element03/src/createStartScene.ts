398b65584458269b5c2d966f3e19c3e7b3a30014
Sabnautica <145000832+Sabnautica@users.noreply.github.com>
2023-12-16 07:02:42 +0000

    Here is my work - Sabir
    B00869306

---
+++ testProj/element03/src/createStartScene.ts
@@ -0,0 +1,316 @@
+//-----------------------------------------------------
+//TOP OF CODE - IMPORTING BABYLONJS
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import {
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    Vector4,
+    HemisphericLight,
+    SpotLight,
+    MeshBuilder,
+    Mesh,
+    Light,
+    Camera,
+    Engine,
+    StandardMaterial,
+    Texture,
+    Color3,
+    Space,
+    ShadowGenerator,
+    PointLight,
+    DirectionalLight,
+    CubeTexture,
+    Sprite,
+    SpriteManager,
+    SceneLoader,
+    ActionManager,
+    ExecuteCodeAction,
+    AnimationPropertiesOverride,
+    CreateSphere,
+  } from "@babylonjs/core";
+  import HavokPhysics from "@babylonjs/havok";
+  import { HavokPlugin, PhysicsAggregate, PhysicsShapeType } from "@babylonjs/core";
+  //----------------------------------------------------
+  
+  //----------------------------------------------------
+  //Initialisation of Physics (Havok)
+  let initializedHavok;
+  HavokPhysics().then((havok) => {
+    initializedHavok = havok;
+  });
+
+  const havokInstance = await HavokPhysics();
+  const havokPlugin = new HavokPlugin(true, havokInstance);
+
+  globalThis.HK = await HavokPhysics();
+  //-----------------------------------------------------
+
+  //MIDDLE OF CODE - FUNCTIONS
+  let keyDownMap: any[] = [];
+  let currentSpeed: number = 0.1;
+  let walkingSpeed: number = 0.1;
+  let runningSpeed: number = 0.4;
+
+  function importPlayerMesh(scene: Scene, collider: Mesh, x: number, y: number) {
+    let tempItem = { flag: false } 
+    let item: any = SceneLoader.ImportMesh("", "./models/", "dummy3.babylon", scene, function(newMeshes, particleSystems, skeletons, animationGroups) {
+      let mesh = newMeshes[0];
+      let skeleton = skeletons[0];
+      skeleton.animationPropertiesOverride = new AnimationPropertiesOverride();
+      skeleton.animationPropertiesOverride.enableBlending = true;
+      skeleton.animationPropertiesOverride.blendingSpeed = 0.05;
+      skeleton.animationPropertiesOverride.loopMode = 1; 
+
+      //adapted from: www.babylonjs-playground.com/#LL5BIQ#0
+      //another good playground for this is: www.babylonjs-playground.com/#AHQEIB#17
+      let idleRange: any = skeleton.getAnimationRange("YBot_Idle");
+      let walkRange: any = skeleton.getAnimationRange("YBot_Walk");
+      // let runRange: any = skeleton.getAnimationRange("YBot_Run");
+      //let leftRange: any = skeleton.getAnimationRange("YBot_LeftStrafeWalk");
+      //let rightRange: any = skeleton.getAnimationRange("YBot_RightStrafeWalk");
+
+      //MOVE THESE IF YOU WANT TO TRIGGER ANYWHERE
+      //let runAnim: any = scene.beginWeightedAnimation(skeleton, runRange.from, runRange.to, 1.0, true);
+      //let leftAnim: any = scene.beginWeightedAnimation(skeleton, leftRange.from, leftRange.to, 1.0, true);
+      //let rightAnim: any = scene.beginWeightedAnimation(skeleton, rightRange.from, rightRange.to, 1.0, true);
+
+      //Speed and Rotation Variables
+      let speed: number = 0.03;
+      let speedBackward: number = 0.01;
+      let rotationSpeed = 0.05;
+
+      //Animation Variables
+      let idleAnim: any;
+      let walkAnim: any;
+      let animating: boolean = false;
+
+      scene.onBeforeRenderObservable.add(()=> {
+        let keydown: boolean = false;
+        if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
+          mesh.moveWithCollisions(mesh.forward.scaleInPlace(speed));                
+          //Previous code
+          //mesh.position.z += 0.01;
+          //mesh.rotation.y = 0;
+          keydown = true;
+        }
+        if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
+          mesh.rotate(Vector3.Up(), -rotationSpeed);
+          //Previous code
+          //mesh.position.x -= 0.01;
+          //mesh.rotation.y = 3 * Math.PI / 2;
+          keydown = true;
+        }
+        if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
+          mesh.moveWithCollisions(mesh.forward.scaleInPlace(-speedBackward));
+          //Previous code
+          //mesh.position.z -= 0.01;
+          //mesh.rotation.y = 2 * Math.PI / 2;
+          keydown = true;
+        }
+        if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
+          mesh.rotate(Vector3.Up(), rotationSpeed);
+          //Previous code
+          //mesh.position.x += 0.01;
+          //mesh.rotation.y = Math.PI / 2;
+          keydown = true;
+        }
+
+        let isPlaying: boolean = false;
+        if (keydown && !isPlaying) {
+          if (!animating) {
+              idleAnim = scene.stopAnimation(skeleton);
+              walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 1.0, true);
+              animating = true;
+          }
+          if (animating) {
+            //walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 1.0, true);
+            isPlaying = true;
+          }
+        } else {
+          if (animating && !keydown) {
+            walkAnim = scene.stopAnimation(skeleton);
+            idleAnim = scene.beginWeightedAnimation(skeleton, idleRange.from, idleRange.to, 1.0, true);
+            animating = false;
+            isPlaying = false;
+          }
+          // if (!animating && !keydown) {
+          //   idleAnim = scene.beginWeightedAnimation(skeleton, idleRange.from, idleRange.to, 1.0, true);
+          // }
+        }
+
+        //collision
+        if (mesh.intersectsMesh(collider)) {
+          console.log("COLLIDED");
+        }
+      });
+
+      //physics collision
+      item = mesh;
+      let playerAggregate = new PhysicsAggregate(item, PhysicsShapeType.CAPSULE, { mass: 0 }, scene);
+      playerAggregate.body.disablePreStep = false;
+
+    });
+    return item;
+  }
+
+  function actionManager(scene: Scene){
+    scene.actionManager = new ActionManager(scene);
+
+    scene.actionManager.registerAction(
+      new ExecuteCodeAction(
+        {
+          trigger: ActionManager.OnKeyDownTrigger,
+          //parameters: 'w'
+        },
+        function(evt) {keyDownMap[evt.sourceEvent.key] = true; }
+      )
+    );
+    scene.actionManager.registerAction(
+      new ExecuteCodeAction(
+        {
+          trigger: ActionManager.OnKeyUpTrigger
+        },
+        function(evt) {keyDownMap[evt.sourceEvent.key] = false; }
+      )
+    );
+    return scene.actionManager;
+  } 
+
+  function createBox(scene: Scene, x: number, y: number, z: number) {
+    let box: Mesh = MeshBuilder.CreateBox("box", { });
+    box.position.x = x;
+    box.position.y = y;
+    box.position.z = z;
+    const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 1 }, scene);
+    return box;
+  }
+    
+  function createGround(scene: Scene) {
+    const ground: Mesh = MeshBuilder.CreateGround("ground", {height: 10, width: 10, subdivisions: 4});
+    const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
+    return ground;
+  }
+
+  //----------------------------------------------------------------------------------------------
+  //Create Skybox
+  function createSkybox(scene: Scene) {
+    //Skybox
+    const skybox = MeshBuilder.CreateBox("skyBox", {size:150}, scene);
+	  const skyboxMaterial = new StandardMaterial("skyBox", scene);
+	  skyboxMaterial.backFaceCulling = false;
+	  skyboxMaterial.reflectionTexture = new CubeTexture("textures/skybox", scene);
+	  skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+	  skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+	  skyboxMaterial.specularColor = new Color3(0, 0, 0);
+	  skybox.material = skyboxMaterial;
+    return skybox;
+  }
+
+
+
+  function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
+    // only spotlight, point and directional can cast shadows in BabylonJS
+    switch (index) {
+      case 1: //hemispheric light
+        const hemiLight = new HemisphericLight("hemiLight", new Vector3(px, py, pz), scene);
+        hemiLight.intensity = 0.1;
+        return hemiLight;
+        break;
+      case 2: //spot light
+        const spotLight = new SpotLight("spotLight", new Vector3(px, py, pz), new Vector3(0, -1, 0), Math.PI / 3, 10, scene);
+        spotLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        let shadowGenerator = new ShadowGenerator(1024, spotLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return spotLight;
+        break;
+      case 3: //point light
+        const pointLight = new PointLight("pointLight", new Vector3(px, py, pz), scene);
+        pointLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        shadowGenerator = new ShadowGenerator(1024, pointLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return pointLight;
+        break;
+    }
+  }
+ 
+  function createHemiLight(scene: Scene) {
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.8;
+    return light;
+  }
+
+  //PREVIOUS METHODS
+  // function createSpotLight(scene: Scene, px: number, py: number, pz: number) {
+  //   var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 10, scene);
+  //   light.diffuse = new Color3(0.39, 0.44, 0.91);
+	//   light.specular = new Color3(0.22, 0.31, 0.79);
+  //   return light;
+  // }
+  
+  function createArcRotateCamera(scene: Scene) {
+    let camAlpha = -Math.PI / 2,
+      camBeta = Math.PI / 2.5,
+      camDist = 10,
+      camTarget = new Vector3(0, 0, 0);
+    let camera = new ArcRotateCamera(
+      "camera1",
+      camAlpha,
+      camBeta,
+      camDist,
+      camTarget,
+      scene,
+    );
+    camera.attachControl(true);
+    return camera;
+  }
+  //----------------------------------------------------------
+  
+  //----------------------------------------------------------
+  //BOTTOM OF CODE - MAIN RENDERING AREA FOR YOUR SCENE
+  export default function createStartScene(engine: Engine) {
+    interface SceneData {
+      scene: Scene;
+      box?: Mesh;
+      ground?: Mesh;
+      importMesh?: any;
+      actionManager?: any;
+      skybox?: Mesh;
+      light?: Light;
+      hemisphericLight?: HemisphericLight;
+      camera?: Camera;
+    }
+  
+    let that: SceneData = { scene: new Scene(engine) };
+    that.scene.debugLayer.show();
+    //initialise physics
+    that.scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
+    //----------------------------------------------------------
+
+    //any further code goes here-----------
+    that.box = createBox(that.scene, 2, 2, 2);
+    that.box = createBox(that.scene, 2, 6, 2);
+    that.box = createBox(that.scene, 2, 15, 2);
+    that.box = createBox(that.scene, 2, 10, 2);
+    that.box = createBox(that.scene, 3, 3, 3);
+    that.box = createBox(that.scene, -4, 3, -4);
+    that.box = createBox(that.scene, -4, 3, 4);
+    that.box = createBox(that.scene, 4, 3, -4);
+    that.box = createBox(that.scene, 0, 8, 0);
+    that.box = createBox(that.scene, 0, 12, 0);
+    that.box = createBox(that.scene, 0, 18, 0);
+    that.ground = createGround(that.scene);
+
+    that.importMesh = importPlayerMesh(that.scene, that.box, 0, 0);
+    that.actionManager = actionManager(that.scene);
+
+    that.skybox = createSkybox(that.scene);
+    //Scene Lighting & Camera
+    that.hemisphericLight = createHemiLight(that.scene);
+    that.camera = createArcRotateCamera(that.scene);
+    return that;
+  }
+  //----------------------------------------------------
